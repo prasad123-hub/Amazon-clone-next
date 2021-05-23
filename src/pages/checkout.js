c8ee26791f040f97260f56f85c401f6334e451c3
Prasad <narkhedkarprasad@gmail.com>
2021-05-23 08:35:20 +0530

    Added checkout button

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -2,13 +2,17 @@ import fs from "fs";
 import Image from "next/image";
 import { useSelector } from "react-redux";
 import Header from "../components/Header";
-import { selectItems } from "../slices/basketSlice";
+import { selectItems, selectTotal } from "../slices/basketSlice";
 import CheckOutProduct from "../components/CheckOutProduct";
+import { signIn, signOut, useSession } from "next-auth/client";
+import Currency from "react-currency-formatter";
+
 // import { useSession } from "next-auth";
 
 function Checkout() {
   const items = useSelector(selectItems);
-  //   const [session] = useSession();
+  const total = useSelector(selectTotal);
+  const [session] = useSession();
 
   return (
     <div className="bg-gray-100">
@@ -45,23 +49,33 @@ function Checkout() {
           </div>
         </div>
         {/*Right */}
-        {/* <div>
+        <div className="flex flex-col bg-white p-10 shadow-md">
           {items.length > 0 && (
             <>
               <h2 className="whitespace-nowrap">
                 Subtotal ({items.length} items):
-                <span className="font-bold"></span>
+                <span className="font-bold">
+                  <Currency quantity={total} Currency="GBP" />
+                </span>
               </h2>
-              <button>
-                {!session ? "SignIn to checkout" : "Proceed to checkout"}
+
+              <button
+                className={`button mt-2 ${
+                  !session &&
+                  `from-gray-300 to-gray-500 border-gray-200 text-gray-300 cursor-not-allowed`
+                }`}
+              >
+                {!session ? "Sign In To Checkout" : "Proceed to Checkout"}
               </button>
             </>
           )}
-        </div> */}
-        <div className="flex flex-col item-cente p-10">
-          <h2 className="whitespace-nowrap mb-2">Subtotal</h2>
-          <button className="button">Checkout</button>
         </div>
+        {/* <div className="flex flex-col item-cente p-10">
+          <h2 className="whitespace-nowrap mb-2">Subtotal</h2>
+          <button disabled={!session} className="button">
+            Checkout
+          </button>
+        </div> */}
       </main>
     </div>
   );
